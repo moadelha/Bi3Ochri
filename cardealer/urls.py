78ee1ca6e38d338ee85cd2d578567f5536b2bdb9
moad <mouad3435@gmail.com>
2024-10-26 14:36:37 +0100

    update static link

--- cardealer/urls.py
+++ cardealer/urls.py
@@ -1,15 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-# from django.conf.urls.static import static
+from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('car.urls')),
+]
 
+# Afixing the photos 
+urlpatterns += staticfiles_urlpatterns() 
 
-] +staticfiles_urlpatterns() 
-
-
-# + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+# Serve media files during development
+if settings.DEBUG:  # Ensure this only runs in development
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
